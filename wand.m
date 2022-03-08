fe695d630b2d2cb7a709cc7a46ec167a1f69e14e
TinaBligh <101092254+TinaBligh@users.noreply.github.com>
2022-03-08 10:46:47 +0100

    Update wand.m

--- wand.m
+++ wand.m
@@ -2,7 +2,7 @@ all:main.o schritte.o antwort.o wand
 wand.o:main.c
   cc -c schritte.c
 schritte.o:schritte.c
-  cc -c antwort.cc
+  cc -c antwort.c
 antwort.o:antwort.c
   cc -c antwort.c
 wand:main.o schritte.o antwort.o
